be951a9e6a8c5fcc5f69962dec6763b8d64f92ad
Edgar <ed.nikogosian@gmail.com>
2023-07-18 17:21:18 +0400

    Added useCallback for remove function

--- src/components/Cards/CardItem.tsx
+++ src/components/Cards/CardItem.tsx
@@ -3,18 +3,16 @@ import styles from './styles.module.css';
 
 type Props = {
   number: number;
-  handleRemove: () => void;
+  handleRemove: (number: number) => void;
 };
 
-const CardItem = ({ number, handleRemove }: Props) => {
-  return (
-    <div className={styles.card}>
-      <button onClick={handleRemove} className={styles.removeButton}>
-        X
-      </button>
-      <p>{number}</p>
-    </div>
-  );
-};
+const CardItem = ({ number, handleRemove }: Props) => (
+  <div className={styles.card}>
+    <button onClick={() => handleRemove(number)} className={styles.removeButton}>
+      X
+    </button>
+    <p>{number}</p>
+  </div>
+);
 
 export default memo(CardItem);
--- src/components/Cards/Cards.tsx
+++ src/components/Cards/Cards.tsx
@@ -1,3 +1,4 @@
+import { useCallback } from 'react';
 import { useAppDispatch, useAppSelector } from '../../hooks';
 import { removeCard } from '../../store/cardsReducer';
 import CardItem from './CardItem';
@@ -7,10 +8,14 @@ const Cards = () => {
   const cardsList = useAppSelector((state) => state.cards.data);
   const dispatch = useAppDispatch();
 
+  const handleRemove = useCallback((number: number) => {
+    dispatch(removeCard(number));
+  }, []);
+
   return (
     <div className={styles.container}>
       {cardsList.map((item) => (
-        <CardItem key={item} number={item} handleRemove={() => dispatch(removeCard(item))} />
+        <CardItem key={item} number={item} handleRemove={handleRemove} />
       ))}
     </div>
   );
